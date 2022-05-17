305eeee13ab345af3981e6caa6df2ce921d91731
izuutech <izutechsofficial@gmail.com>
2022-05-17 09:43:27 +0100

    edit start text

--- index.js
+++ index.js
@@ -38,33 +38,46 @@ app.post(URI, async (req, res)=>{
 
     //send a fetch request to the dictionary api to get meaning
     // console.log(text.split(" ")[0])
-    try{
-        const dictionaryWord=await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text.split(" ")[0]}`);
-        
-        if(dictionaryWord.data){
-            const definitions=dictionaryWord?.data[0].meanings[0].definitions;
-            let x=1;
-            let eachDefiniition=definitions.map(meaning=>{
-                return `${x++}.)  ${meaning.definition} \n \n`
-            })
+    if(text!="/start"){
+        try{
+            const dictionaryWord=await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text.split(" ")[0]}`);
             
-            await axios.post(`${TELEGRAM_API}/sendMessage`, {
-                chat_id: chatId,
-                text: `The definition(s) for "${text}" is/are \n \n ${eachDefiniition.toString()}`
-            })
-            return res.send()
-        }else{
-            await axios.post(`${TELEGRAM_API}/sendMessage`, {
-                chat_id: chatId,
-                text: "No definitions"
-            })
-            return res.send()
+            if(dictionaryWord.data){
+                const definitions=dictionaryWord?.data[0].meanings[0].definitions;
+                let x=1;
+                let eachDefiniition=definitions.map(meaning=>{
+                    return `${x++}.)  ${meaning.definition} \n \n`
+                })
+                
+                await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                    chat_id: chatId,
+                    text: `The definition(s) for "${text}" is/are \n \n ${eachDefiniition.toString()}`
+                })
+                return res.send()
+            }else{
+                await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                    chat_id: chatId,
+                    text: "No definitions"
+                })
+                return res.send()
+            }
+        }catch(err){
+            try{
+                await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                    chat_id: chatId,
+                    text: `${text} is not an english word`
+                })
+                return res.send()
+            }catch(e){
+                console.log(e)
+            }
         }
-    }catch(err){
+    }else{
         try{
             await axios.post(`${TELEGRAM_API}/sendMessage`, {
                 chat_id: chatId,
-                text: `${text} is not an english word`
+                text: `Welcome to Dictionary Bot. \n
+                I will define every word you give to me. \n You contact my developer via www.joshuaizu.vercel.app`
             })
             return res.send()
         }catch(e){
