43ebd6271354fa39b6480ee935e310676f405878
izuutech <izutechsofficial@gmail.com>
2022-05-17 09:07:07 +0100

    add heroku configs4

--- index.js
+++ index.js
@@ -31,47 +31,42 @@ const init=async()=>{
 }
 
 app.post(URI, async (req, res)=>{
-    console.log(req.body)
-    console.log(WEBHOOK_URL)
+    //console.log(req.body)
     const chatId=req.body.message.chat.id;
     const text=req.body.message.text;
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: `${text} is not an english word`
-    })
-    return res.send()
+    
 
     //send a fetch request to the dictionary api to get meaning
     // console.log(text.split(" ")[0])
-    // try{
-    //     const dictionaryWord=await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text.split(" ")[0]}`);
+    try{
+        const dictionaryWord=await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text.split(" ")[0]}`);
         
-    //     if(dictionaryWord.data){
-    //         const definitions=dictionaryWord?.data[0].meanings[0].definitions;
-    //         let x=1;
-    //         let eachDefiniition=definitions.map(meaning=>{
-    //             return `${x++}.)  ${meaning.definition} \n \n`
-    //         })
+        if(dictionaryWord.data){
+            const definitions=dictionaryWord?.data[0].meanings[0].definitions;
+            let x=1;
+            let eachDefiniition=definitions.map(meaning=>{
+                return `${x++}.)  ${meaning.definition} \n \n`
+            })
             
-    //         await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    //             chat_id: chatId,
-    //             text: `The definition(s) for "${text}" is/are \n \n ${eachDefiniition.toString()}`
-    //         })
-    //         return res.send()
-    //     }else{
-    //         await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    //             chat_id: chatId,
-    //             text: "No definitions"
-    //         })
-    //         return res.send()
-    //     }
-    // }catch(err){
-    //     await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    //         chat_id: chatId,
-    //         text: `${text} is not an english word`
-    //     })
-    //     return res.send()
-    // }
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: chatId,
+                text: `The definition(s) for "${text}" is/are \n \n ${eachDefiniition.toString()}`
+            })
+            return res.send()
+        }else{
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: chatId,
+                text: "No definitions"
+            })
+            return res.send()
+        }
+    }catch(err){
+        await axios.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: chatId,
+            text: `${text} is not an english word`
+        })
+        return res.send()
+    }
 })
 
 
